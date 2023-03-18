d533734b0753c2339b2bc82fa35a2f101b75570b
Dgo45921 <3023135080101@ingenieria.usac.edu.gt>
2023-03-17 19:21:08 -0600

    avances en la gui y funcionalidad del login

---
+++ EDD_Proyecto1_Fase2/js/main.js
@@ -0,0 +1,26 @@
+const loginButton = document.getElementById("login_button");
+
+loginButton.addEventListener("click", function() {
+    login()
+});
+
+
+function login(){
+    const username = document.getElementById("username_input").value.replace(" ", "");
+    const password = document.getElementById("password_input").value.replace(" ", "");
+   // console.log(password)
+   // console.log(username)
+
+    if (username === "" || password === "" ){
+        alert("Por favor, llene todos los campos")
+    }
+
+    else if (username === "admin" && password === "admin" ){
+        alert("Bienvenido admin")
+    }
+    else{
+        console.log("busco en el arbol avl")
+    }
+
+
+}
